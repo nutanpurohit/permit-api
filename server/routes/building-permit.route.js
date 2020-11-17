ad5bd67bacb9307f57beddf73b8b378c9545de02
nutanpurohit <nutanpurohit37@gmail.com>
2020-11-17 19:27:09 +0530

    small change

--- server/routes/building-permit.route.js
+++ server/routes/building-permit.route.js
@@ -11,9 +11,10 @@ router.route('/')
 router.route('/:id')
 
     /** GET /api/{version}/building-permit/:id - operations for specific building permit */
-    .get(buildingPermitCtrl.get)
+    .get(buildingPermitCtrl.get);
 
-    /** PUT /api/{version}/staff/building-permit - Update building permit info by the staff users */
+router.route('/staff/:id')
+    /** PUT /api/{version}/building-permit/staff/:id - Update building permit info by the staff users */
     .put(buildingPermitCtrl.updatePermitByStaff);
 
 export default router;
